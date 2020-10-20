dfcf362eb0494df831f0c952e6cf4e3f3f6f3e99
Arnau <agomez1f@gmail.com>
2020-10-20 23:15:24 +0200

    Added redirection, routing, file system

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  ****************************/
 
 const http = require('http');
+const fs = require('fs');
 
 /**
  * Node.js is based on an EVENT-DRIVEN ARCHITECTURE. That means, most of the code is made of
@@ -15,7 +16,32 @@ const http = require('http');
 
 const rqListener = (req, res) => {
   console.log(req.url, req.method, req.headers);
-
+  const {url, method} = req;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html'); // indica el tipus de document que enviarem.
+    res.write(
+      `<html>
+        <head>
+          <title>Look mom my first website with node js yaaaay</title>
+        </head>
+        <body>
+          <h1>Look mom my first website with node.js yaaaay</h1>
+          <form action="/message" method="POST" name="message">
+            <input type="text">
+            <button>Send</button>
+          </form>
+        </body>
+      </html>`
+    ); // you can also call res.write() many times, for example, one for every line of HTML
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    /* Takes the post request created in the form and writes it into a new file */
+    fs.writeFileSync('message.txt', 'Example text');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html'); // indica el tipus de document que enviarem.
   res.write(
     `<html>
@@ -27,7 +53,7 @@ const rqListener = (req, res) => {
       </body>
     </html>`
   ); // you can also call res.write() many times, for example, one for every line of HTML
-  res.end() // finishes preparing the response and returns it to the server. 
+  res.end(); // finishes preparing the response and returns it to the server.
   //process.exit(); // exits the event loop
 };
 
@@ -38,7 +64,7 @@ const rqListener = (req, res) => {
 const server = http.createServer(rqListener); // this function takes a request listener as argument
 
 /*==============================
- * MAKE THE SERVER LISTEN
+ *  MAKE THE SERVER LISTEN
  *=============================*/
 
 // once the server is created, this function will keep the server running till it listens to the request.
